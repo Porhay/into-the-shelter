30f81f4af62ecff3ec6ff69a2eb550e7feaf3573
Denys Mitiachkin <denismityachkin@gmail.com>
2024-01-28 00:29:05 +0200

    add webcam

---
+++ apps/shelter-client/src/libs/Webcam.tsx
@@ -0,0 +1,11 @@
+import Webcam from "react-webcam";
+
+const CustomWebcam = () => {
+  return (
+    <div className="container">
+      <Webcam height={600} width={600} />
+    </div>
+  );
+};
+
+export default CustomWebcam;
