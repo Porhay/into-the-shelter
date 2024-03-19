ef6b4f044dc6a91bc6b33a98fae7e6ac906c7ed0
Denys Mitiachkin <denismityachkin@gmail.com>
2024-03-19 20:03:43 +0200

    SH-54 Implement loader (#36)

--- apps/shelter-client/src/App.tsx
+++ apps/shelter-client/src/App.tsx
@@ -1,21 +1,21 @@
 import './styles/App.scss';
-import Navigation from './components/Navigation';
+import { useSelector } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
+import Navigation from './components/Navigation';
 import Router from './components/Router';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { RootState } from './redux/store';
+import Loader from './libs/loader';
 
 
 function App() {
+  const app = useSelector((state: RootState) => state.app);
   return (
-    <Provider store={store}>
-      <div className='App'>
-        <BrowserRouter>
-          <Navigation />
-          <Router />
-        </BrowserRouter>
-      </div>
-    </Provider>
+    <div className='App'>
+      <BrowserRouter>
+        <Navigation />
+        {app.loading ? <Loader /> : <Router />}
+      </BrowserRouter>
+    </div>
   );
 }
 
