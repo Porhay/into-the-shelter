c5415d58e027e9a9d86447e37a7b9cf9a2b72817
Denys Mitiachkin <denismityachkin@gmail.com>
2024-03-24 18:53:58 +0200

    fix docker issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
--- apps/shelter-client/.eslintrc.js
+++ apps/shelter-client/.eslintrc.js
@@ -1,6 +1,6 @@
 // import baseConfig from '../../.eslintrc.js';
 
-export default {
+module.exports = {
   // ...baseConfig,
   root: false,
 };
